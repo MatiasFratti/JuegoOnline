fbb04eef832268664355d2099d748cc47f20df20
MatiasFratti <50062246+MatiasFratti@users.noreply.github.com>
2020-09-27 20:46:51 -0300

    Update database.js

--- HDP_Backend/src/database.js
+++ HDP_Backend/src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb+srv://Matias:ojDXYziPcxCGsNSc@cluster0-pb36h.mongodb.net/Cluster0?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://********************/Cluster0?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useFindAndModify:false,
